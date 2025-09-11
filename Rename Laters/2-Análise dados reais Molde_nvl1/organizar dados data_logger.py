2a0b91937219f256e7ea7aceacd9e8b013108b93
Alexandre Jesus Andrade da Costa <150287535+Katph0bia@users.noreply.github.com>
2025-09-11 01:09:09 +0000

    Scripts 3

--- Rename Laters/2-Análise dados reais Molde_nvl1/organizar dados data_logger.py
+++ Rename Laters/2-Análise dados reais Molde_nvl1/organizar dados data_logger.py
@@ -8,24 +8,24 @@ Created on Mon Jun 16 16:44:54 2025
 
 import pandas as pd
 
-# Caminho para o ficheiro original CSV
+# Original file Path CSV
 ficheiro_csv = r'C:/Users/ugims/inegi.up.pt/Teses & Estágios - Teses_Estágios - Miguel António Costa - Teses_Estágios - Miguel António Costa/3. Repositório do Miguel/Miguel_MoldeQuente/Session_6M2.csv'  # certifica-te de que está no mesmo diretório do script ou ajusta o caminho
 
-# Lê o conteúdo todo do ficheiro, linha a linha, como texto
+# Read file, line by line
 with open(ficheiro_csv, 'r', encoding='utf-8') as f:
     linhas = [linha.strip().split(',') for linha in f.readlines()]
 
-# Determina o número máximo de colunas encontradas (para alinhar tudo)
+# Find collumns (for alignment)
 max_colunas = max(len(linha) for linha in linhas)
 
-# Preenche com células vazias onde faltar informação
+# Fill empty cells
 linhas_alinhadas = [linha + [''] * (max_colunas - len(linha)) for linha in linhas]
 
-# Converte para DataFrame
+# Convert to Dataframe
 df_bruto = pd.DataFrame(linhas_alinhadas)
 
-# Guarda no Excel exatamente como estava
+# Save to Excel
 df_bruto.to_excel("Dados_MIGUEL_Bruto_3.xlsx", index=False, header=False)
 
-print("Ficheiro 'Dados_MIGUEL_Bruto.xlsx' criado com todos os dados originais.")
+print("File 'Dados_MIGUEL_Bruto.xlsx' created with all original data.")
 
